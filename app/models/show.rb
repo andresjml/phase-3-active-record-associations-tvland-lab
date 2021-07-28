0ad7eeb5877e6d062fd48def27304519cce0576f
Andres <andresjml@gmail.com>
2021-07-28 15:48:10 -0400

    comleted assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    has_many :actors, through: :characters
+    belongs_to :network
+    has_many :characters
+
+
+
+  def actors_list
+    self.actors.map {|actor|actor.full_name}
+  end
 end
